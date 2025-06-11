f40663d14acbbd93718a3891094b35a430257ee9
Maria de Jesus <m.jesus@escolar.ifrn.edu>
2025-06-11 17:50:51 -0300

    maria_de_jesus_prova01

--- testee/src/App.jsx
+++ testee/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Card from '../../aula05/src/componentes/Card.jsx';
+import Detalhes from '../../aula05/src/componentes/Detalhes.jsx';
+import Tema from '../../aula05/src/componentes/Tema.jsx';
+import Galeria from '../../aula05/src/componentes/Galeria.jsx';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+const characters = [
+  {
+    id: 1,
+    name: 'cachorro',
+    description: 'auau.',
+    image: cachorro1
+  },
+  {
+    id: 2,
+    name: 'auau',
+    description: 'bb.',
+    image: cachorro2
+  },
+   {
+    id: 3,
+    name: 'cachorrin',
+    description: 'lindinho.',
+    image: cachorro3
+  },
+  {
+    id: 4,
+    name: 'fofin',
+    description: 'carissimo.',
+    image: cachorro4
+  },
+   {
+    id: 5,
+    name: 'ararinha',
+    description: 'amostrada.',
+    image: arara1
+  },
+  {
+    id: 6,
+    name: 'arara',
+    description: 'linda.',
+    image: arara2
+  }
+
+];
+
+export default function App() {
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Tema>
 
-export default App
+      <h1>Galeria de Personagens</h1>
+      
+      <Galeria characters={characters}/>
+    </Tema>
+  );
+}
